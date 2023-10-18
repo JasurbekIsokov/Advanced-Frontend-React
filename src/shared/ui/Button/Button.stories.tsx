0fa419e310b3220b35cf302315dbaec9bfb988ec
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-18 10:04:48 +0500

    Configuration storybook and Button storyes

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -7,17 +7,29 @@ import { Theme } from "app/providers/ThemeProvider";
 export default {
   title: "shared/Button",
   component: Button,
+  // component: Test qilingan komponent. Storybook ko'rsatuvchi oynasi uchun
+  // test qilingan komponentni aniqlaydi.
+
   argTypes: {
     backgroundColor: { control: "color" },
   },
+  // argTypes: Komponentning argumentlarini tavsiflash uchun ishlatiladi. Misol uchun, bu yerda "backgroundColor" argumenti komponentni xususiy rangini o'zgartirish uchun ishlatiladi.
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+// Template: Komponentni ko'rsatuvchi funksiya. Ushbu funksiya qo'shilgan komponentni interfeysda ko'rsatish uchun ishlatiladi.
+
+// ComponentStory va ComponentMeta obyektlari: Bu obyektlar Storybook
+// komponentlarini yaratish va tavsiflashda foydalaniladi. ComponentStory
+// komponentlarni yaratishda ishlatiladi, ComponentMeta esa komponentlarning
+// tavsifi uchun foydalaniladi.
 
 export const Primary = Template.bind({});
 Primary.args = {
   children: "Text",
 };
+
+// Primary, Clear, Outline, va OutlineDark: Bu, ko'rsatuvchi versiyalari (story) bo'lgan komponentlar. Ularning har biri bir xususiy tema (ButtonTheme) va "children" nomli yerga joylashtirilgan tekst bilan tanimlangan. Bu komponentlar ko'rsatuvchi oynasida namoyish etiladi va ularga xususiy xususiyatlar berilgan.
 
 export const Clear = Template.bind({});
 Clear.args = {
@@ -38,3 +50,4 @@ OutlineDark.args = {
 };
 
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+// OutlineDark.decorators: Ushbu qismat, "OutlineDark" versiyasini o'zgartirilgan temaga (Theme.DARK) o'zgartirish uchun ishlatiladi. Tema o'zgartirilgan versiyasini sinash uchun foydalaniladi.
