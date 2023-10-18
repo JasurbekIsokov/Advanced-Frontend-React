3701fb6307303d3ec9f8b77b29deed66d0ce1e12
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-18 16:49:36 +0500

    test commit

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,15 +3,15 @@ import path from "path";
 import { buildCssLoader } from "../build/loaders/buildCssLoader";
 import { BuildPaths } from "../build/types/config";
 
-export default ({ config }: { config: webpack.Configuration }) => {
+export default ({ config }) => {
   const paths: BuildPaths = {
     build: "",
     html: "",
     entry: "",
     src: path.resolve(__dirname, "..", "..", "src"),
   };
-  config.resolve?.modules?.push(paths.src);
-  config.resolve?.extensions?.push(".ts", ".tsx");
+  config.resolve.modules.push(paths.src);
+  config.resolve.extensions?.push(".ts", ".tsx");
 
   // eslint-disable-next-line no-param-reassign
   // @ts-ignore
@@ -22,13 +22,13 @@ export default ({ config }: { config: webpack.Configuration }) => {
     return rule;
   });
 
-  config.module?.rules.push({
+  config.module.rules.push({
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   });
-  config.module?.rules.push(buildCssLoader(true));
+  config.module.rules.push(buildCssLoader(true));
 
-  config.plugins?.push(
+  config.plugins.push(
     new DefinePlugin({
       IS_DEV: JSON.stringify(true),
       API: JSON.stringify(""),
