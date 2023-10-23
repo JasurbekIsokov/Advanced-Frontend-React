702a275c089ea76fc4d8c5ab9f33d96b01c0d351
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-23 09:17:32 +0500

    Fixed myl

--- build/196.f6f21c7d5895ca78ed48.js
+++ build/196.f6f21c7d5895ca78ed48.js
@@ -1,1 +1,8 @@
-"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[196],{4196:(t,e,r)=>{r.r(e),r.d(e,{default:()=>o});var c=r(5893),u=r(7294),n=r(2327);const o=function(){var t=(0,n.$)().t,e=(0,u.useState)("");return e[0],e[1],(0,c.jsx)("div",{children:t("Главная страница")})}}}]);
+(self.webpackChunkproduction_project = self.webpackChunkproduction_project || []).push([[196], {
+  4196: (t, e, r) => {
+    r.r(e), r.d(e, { default: () => o }); const c = r(5893); const u = r(7294); const n = r(2327); const o = function () {
+      const { t } = (0, n.$)(); const
+        e = (0, u.useState)(''); return e[0], e[1], (0, c.jsx)('div', { children: t('Главная страница') });
+    };
+  },
+}]);
