5c18f292379aa062d07c3ab66c4252e4b67f5b45
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-24 11:41:56 +0500

    ReWrite All codes in Project

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,37 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC, memo, ReactNode } from 'react';
+import cls from './AppLink.module.scss';
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    RED = 'red',
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string;
+    theme?: AppLinkTheme;
+    children?: ReactNode;
+}
+
+export const AppLink = memo((props: AppLinkProps) => {
+    const {
+        to,
+        className,
+        children,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props;
+
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, { [cls[theme]]: true }, [className])}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+});
