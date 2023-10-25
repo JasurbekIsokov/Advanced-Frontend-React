f96c850a8c6771147d8201e4b44def1e3cf3463c
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-25 11:15:39 +0500

    Fixed Style Profile Card

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -16,6 +16,7 @@ const options = [
     { value: Currency.RUB, content: Currency.RUB },
     { value: Currency.EUR, content: Currency.EUR },
     { value: Currency.USD, content: Currency.USD },
+
 ];
 
 export const CurrencySelect = memo(({
