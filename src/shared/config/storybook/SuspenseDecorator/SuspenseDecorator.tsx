fe2a03b9b561f866393bedf1fe8d4f0336dbfddc
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-11-01 13:59:23 +0500

    useModal. Refactoring Modal and Drawer. && Dynamic import. Lazy libraries. Provider. Gesture, React spring LABEL

--- src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator.tsx
+++ src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator.tsx
@@ -1,9 +1,8 @@
 import { Story } from '@storybook/react';
-import { Loading } from 'entities/Article/ui/ArticleDetails/ArticleDetails.stories';
 import { Suspense } from 'react';
 
 export const SuspenseDecorator = (StoryComponent: Story) => (
-    <Suspense fallback={<Loading id="" />}>
+    <Suspense>
         <StoryComponent />
     </Suspense>
 );
